b25146789a6586dc9d50e3c8f9c23a7e911469f8
Luis <luismanlaz@hotmail..com>
2023-03-23 14:16:10 -0300

    23/03/2023

--- screens/form.js
+++ screens/form.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, StyleSheet } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, TextInput } from 'react-native'
+import CheckBox from 'react-native-check-box'
 import { Formik } from 'formik';
 import * as yup from 'yup';
 
@@ -8,7 +9,34 @@ const Form = () => {
 
     return (
         <View style={styles.container}>
-            <Text>form</Text>
+            <Formik>
+                <View>
+                    <TextInput
+                        placeholder='Title*'
+                    />
+                    <TextInput
+                        placeholder='Summary*'
+                    />
+                    <TextInput
+                        placeholder='Steps'
+                    />
+                    <TextInput
+                        placeholder='Image url'
+                    />
+                    <TextInput
+                        placeholder='Health Score'
+                    />
+                    <TextInput
+                        placeholder='Ready in (minutes)'
+                    />
+                    <View>
+                        <CheckBox
+                            onClick={() => {
+                            }}
+                        />
+                    </View>
+                </View>
+            </Formik>
         </View>
     )
 }
